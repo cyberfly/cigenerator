99a29a89a8adee6ddd61c864e0b2b66633d9c78f
Fathur <mhd.fathur@uvsms.com>
2013-04-22 10:12:36 +0800

    try update the config file

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'devadmin';
+$db['default']['password'] = 'devadmin123';
+$db['default']['database'] = 'devadmin_erepair';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
