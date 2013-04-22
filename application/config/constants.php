33cc0b944f089736ee9968d1abe36c51e139af8d
Fathur <mhd.fathur@uvsms.com>
2013-04-22 11:03:34 +0800

    delete some line

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,53 +36,5 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
-//constant to check member in which group
-define('ADMIN','admin');
-define('PUBLISHER','publisher');
-define('CUSTOMER','customer');
-define('CUSTOMER_ROLE',2);
-
-##Payment
-define('PAYMENT_STEPS_PENDING',0);
-define('PAYMENT_STEPS_SENT_AND_WAIT',1);
-define('PAYMENT_STEPS_REGISTER',2);
-define('PAYMENT_STEPS_SUCCESS',4);
-
-//members group
-define('ADMIN_GROUP',1);
-define('PUBLISHER_GROUP',3);
-define('CUSTOMER_GROUP',4);
-
-define('VIDEO_LIST_PER_PAGE', 80);
-
-##All related to video
-define('PAY_PERV_VIEW_DURATION', '31'); ##means 31 days
-
-define('HQ_EMAIL','charles.ling@alphacrossing.com');
-
-##New registration secret hash key
-define('SECRET_PASSWORD_SALT','Jloap17$%78930n<ma5n-234KiaoqfaJvhcsBTY');
-
-##Secret video image link hash
-define('SECRET_IMG_LINK_SALT','PQqwme*7234sudcnMI*%45y28BAtqj4hjghnMna');
-
-##Secret video link hash
-define('SECRET_VID_LINK_SALT',')9123kBsNjkiCnN#6715G31w9KaQlcxre91V3cjas');
-
-##secret video hotlinking
-define('SECRET_VID_HOTLINKING', 'Opa8118271badgqGa1tin71Hag29mIKajsaq');
-
-define('SECRET_CREDIT_SALT', '192A83nT^76nsNha&*12noPp01926A35sAMn7908Had');
-
-##General hash
-define('SECRET_GENERAL_SALT','MkawYt638*7216NbAhdTkChErd62930cfpolqwMncn');
-define('CHANNELS_HASH', 'Is8192nMlpaYq6&12sbqiolamLpsqj212nxan');
-
-define('RECAPTCHA_PRIVATE_KEY','6LcICNASAAAAAKXWmm1lkXIezvNWSYsZAWk7rHJ4');
-
-##Server Library path
-define('LOCAL_SERVER_PATH', $_SERVER['DOCUMENT_ROOT'].'/staging/charles/video/');  
-
-
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
